b480a8c7e0c217e3838f9c3a86008e000a8dfb8b
IHEEB <132073063+IHEEBEU@users.noreply.github.com>
2023-08-25 12:21:46 +0100

    Update config.js
    hosting

--- back-end/db/config.js
+++ back-end/db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb://127.0.0.1:27017/blog-app', {
+mongoose.connect('mongodb+srv://mongo:iheb1234@cluster0.fowqa3m.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
