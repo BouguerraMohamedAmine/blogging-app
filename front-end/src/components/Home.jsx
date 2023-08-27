3bad9bba577d896bb29a5c6b387b1e11c12d3298
Bouguerra Med.Amine <132056227+BouguerraMohamedAmine@users.noreply.github.com>
2023-08-27 19:34:06 +0100

    done!!!!!!!!!!

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UsersView from './UsersView';
 
 function Home({ followed, searched }) {
   console.log("hey there", followed);
-  console.log("heehu",searched);
-  const searchedArray = Object.values(searched);
-console.log(searchedArray)
+  console.log("heehu", searched);
+  
+  // Determine which section to display: 'home' or 'searching'
+  const [activeSection, setActiveSection] = useState(followed.length > 0 ? 'home' : 'searching');
+
+  useEffect(() => {
+    // Determine which section to activate based on the last change
+    if (followed.length > 0) {
+      setActiveSection('home');
+    } else {
+      setActiveSection('searching');
+    }
+  }, [followed, searched]);
+
   return (
     <div className="home">
-      {followed.map((e, index) => (
-        <div className='main-home' key={index}>
-          <img className='home-pic' src={e.image} alt={`User ${index} Image`} />
-          <h2 className='home-title'> {e.title} </h2>
-          <h2 className='home-body'>{e.body}</h2>
+      {activeSection === 'home' && (
+        <div className='home'>
+          {followed.map((e, index) => (
+            <div className='main-home' key={index}>
+              <img className='home-pic' src={e.image} alt={`User ${index} Image`} />
+              <h2 className='home-title'> {e.title} </h2>
+              <h2 className='home-body'>{e.body}</h2>
+            </div>
+          ))}
         </div>
-      ))}
-      
-      <div className='searching'>
-        {searchedArray.map((e, index) => (
-          <div key={index}>
-            <img src={e.image} alt={`Search Result ${index} Image`} />
-            <h2>{e.username}</h2>
-          </div>
-        ))}
-      </div>
+      )}
+
+      {activeSection === 'searching' && (
+        <div className='home  '>
+          {Object.values({searched}).map((e, index) => (
+            <div className='searching' key={index}>
+              <img className='home-pic' src={e.image} alt={`Search Result ${index} Image`} />
+              <h2 className='home-title' > {e.username} </h2>
+              <h2 className='home-body'>{e.phoneNumber} </h2>
+              <h2 className='home-body'> {e.city}</h2>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
