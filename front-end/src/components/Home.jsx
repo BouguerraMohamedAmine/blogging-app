628ea43f7436d15f0f8217bfb359e0a4f7f0bee1
Bouguerra Med.Amine <132056227+BouguerraMohamedAmine@users.noreply.github.com>
2023-08-27 19:00:49 +0100

    search

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import UsersView from './UsersView';
 
-function Home({ followed , searchResults }) {
+function Home({ followed, searched }) {
   console.log("hey there", followed);
-console.log ('haw search li theb alih w sayebni' , searchResults)
+  console.log("heehu",searched);
+  const searchedArray = Object.values(searched);
+console.log(searchedArray)
   return (
     <div className="home">
+      {followed.map((e, index) => (
+        <div className='main-home' key={index}>
+          <img className='home-pic' src={e.image} alt={`User ${index} Image`} />
+          <h2 className='home-title'> {e.title} </h2>
+          <h2 className='home-body'>{e.body}</h2>
+        </div>
+      ))}
       
-      {
-        followed.map((e, index) => (
-          <div className='main-home' key={index}>
-          <img className='home-pic' src={e.image}/>
-          <h2 className='home-title'> {e.title}   </h2>
-          <h2 className='home-body'>
-          {e.body}
-
-            </h2>
+      <div className='searching'>
+        {searchedArray.map((e, index) => (
+          <div key={index}>
+            <img src={e.image} alt={`Search Result ${index} Image`} />
+            <h2>{e.username}</h2>
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
   );
 }
